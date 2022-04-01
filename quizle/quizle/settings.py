e242555d8f7eaf7d154dc96ecbe7d22497c23f31
Brandon Samaroo <samaroo.bran@gmail.com>
2022-04-01 12:18:25 -0400

    fixed settings

--- quizle/quizle/settings.py
+++ quizle/quizle/settings.py
@@ -35,7 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'main_app'
+    'main_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
