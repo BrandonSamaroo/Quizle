48dbeec2ed6f19142c95814804bf3d2fd96351dc
Brandon Samaroo <samaroo.bran@gmail.com>
2022-04-08 09:39:41 -0400

    finished

--- quizle/settings.py
+++ quizle/settings.py
@@ -81,9 +81,9 @@ WSGI_APPLICATION = 'quizle.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('NAME'),
-        'USER': os.getenv('USER'),
-        'PASSWORD': os.getenv('PASSWORD')
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': ''
     }
 }
 
@@ -134,3 +134,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import django_heroku
+django_heroku.settings(locals())
